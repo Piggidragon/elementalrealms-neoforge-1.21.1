0f15227c857fb7d57782e56babd21206f44cdc3b
Piggidragon <inf24119@lehre.dhbw-stuttgart.de>
2025-11-10 22:07:21 +0100

    feat: added custom keybind

---
+++ src/main/java/de/piggidragon/elementalrealms/events/ClientKeyInputHandler.java
@@ -0,0 +1,28 @@
+package de.piggidragon.elementalrealms.events;
+
+import de.piggidragon.elementalrealms.packets.OpenAffinityGuiPacket;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
+
+/**
+ * Handles client-side key input events
+ */
+@EventBusSubscriber(modid = "elementalrealms", value = Dist.CLIENT)
+public class ClientKeyInputHandler {
+
+    /**
+     * Checks for key presses every client tick
+     * @param event The client tick event
+     */
+    @SubscribeEvent
+    public static void onClientTick(ClientTickEvent.Post event) {
+        // Check if affinity GUI key was pressed
+        while (ModKeyBindingsHandler.OPEN_AFFINITY_GUI.consumeClick()) {
+            // Send packet to server to open the menu
+            PacketDistributor.sendToServer(new OpenAffinityGuiPacket());
+        }
+    }
+}
---
+++ src/main/java/de/piggidragon/elementalrealms/events/ModKeyBindingsHandler.java
@@ -0,0 +1,35 @@
+package de.piggidragon.elementalrealms.events;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import org.lwjgl.glfw.GLFW;
+
+/**
+ * Registers custom key bindings for the mod
+ */
+@EventBusSubscriber(modid = "elementalrealms", value = Dist.CLIENT)
+public class ModKeyBindingsHandler {
+
+    /**
+     * Key binding to open the affinity GUI
+     */
+    public static final KeyMapping OPEN_AFFINITY_GUI = new KeyMapping(
+            "key.elementalrealms.open_affinity_gui", // Translation key
+            InputConstants.Type.KEYSYM,               // Key type (keyboard)
+            GLFW.GLFW_KEY_K,                         // Default key (K)
+            "key.categories.elementalrealms"         // Category translation key
+    );
+
+    /**
+     * Registers all key mappings
+     * @param event The registration event
+     */
+    @SubscribeEvent
+    public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
+        event.register(OPEN_AFFINITY_GUI);
+    }
+}
