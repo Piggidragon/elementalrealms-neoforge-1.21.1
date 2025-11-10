e5cefe1c5a7d8e7a15c1d0d288e988730700b3a7
Piggidragon <inf24119@lehre.dhbw-stuttgart.de>
2025-11-10 15:59:19 +0100

    fix: adjusted GenerationCenterData to 1.21.1

--- src/main/java/de/piggidragon/elementalrealms/saveddata/GenerationCenterData.java
+++ src/main/java/de/piggidragon/elementalrealms/saveddata/GenerationCenterData.java
@@ -2,12 +2,14 @@ package de.piggidragon.elementalrealms.saveddata;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.saveddata.SavedData;
-import net.minecraft.world.level.saveddata.SavedDataType;
 import net.minecraft.world.level.storage.DimensionDataStorage;
 
 import java.util.HashMap;
@@ -19,22 +21,26 @@ import java.util.Map;
  */
 public class GenerationCenterData extends SavedData {
 
-    // Codec for ResourceKey<Level> to ChunkPos mapping
+    // Custom codec for ChunkPos (x, z coordinates)
+    private static final Codec<ChunkPos> CHUNK_POS_CODEC = RecordCodecBuilder.create(
+            instance -> instance.group(
+                    Codec.INT.fieldOf("x").forGetter(pos -> pos.x),
+                    Codec.INT.fieldOf("z").forGetter(pos -> pos.z)
+            ).apply(instance, ChunkPos::new)
+    );
+
+    // Codec for the entire data structure
     private static final Codec<GenerationCenterData> CODEC = RecordCodecBuilder.create(
             instance -> instance.group(
                     Codec.unboundedMap(
                             Level.RESOURCE_KEY_CODEC,
-                            ChunkPos.CODEC
+                            CHUNK_POS_CODEC  // Use custom ChunkPos codec
                     ).fieldOf("generationCenters").forGetter(data -> data.generationCenters),
                     Codec.INT.fieldOf("layer").forGetter(data -> data.layer)
             ).apply(instance, GenerationCenterData::new)
     );
 
-    private static final SavedDataType<GenerationCenterData> TYPE = new SavedDataType<>(
-            "generation_centers",
-            GenerationCenterData::new,
-            CODEC
-    );
+    private static final String DATA_NAME = "generation_centers";
 
     // Map of dimension to its generation center
     private final Map<ResourceKey<Level>, ChunkPos> generationCenters;
@@ -67,7 +73,48 @@ public class GenerationCenterData extends SavedData {
      */
     public static GenerationCenterData get(MinecraftServer server) {
         DimensionDataStorage storage = server.overworld().getDataStorage();
-        return storage.computeIfAbsent(TYPE);
+        return storage.computeIfAbsent(
+                new SavedData.Factory<>(
+                        GenerationCenterData::new,  // Supplier for new data
+                        GenerationCenterData::load, // Function to load from NBT
+                        null                         // DataFixType (optional)
+                ),
+                DATA_NAME
+        );
+    }
+
+    /**
+     * Loads the data from NBT using the codec.
+     *
+     * @param tag            The NBT compound tag
+     * @param lookupProvider Registry lookup provider
+     * @return The loaded GenerationCenterData
+     */
+    public static GenerationCenterData load(CompoundTag tag, HolderLookup.Provider lookupProvider) {
+        return CODEC.parse(lookupProvider.createSerializationContext(NbtOps.INSTANCE), tag)
+                .resultOrPartial(error -> {
+                    // Log error if parsing fails
+                    System.err.println("Failed to load GenerationCenterData: " + error);
+                })
+                .orElseGet(GenerationCenterData::new);
+    }
+
+    /**
+     * Saves the data to NBT using the codec.
+     *
+     * @param tag            The NBT compound tag to write to
+     * @param lookupProvider Registry lookup provider
+     * @return The modified compound tag
+     */
+    @Override
+    public CompoundTag save(CompoundTag tag, HolderLookup.Provider lookupProvider) {
+        CODEC.encodeStart(lookupProvider.createSerializationContext(NbtOps.INSTANCE), this)
+                .resultOrPartial(error -> {
+                    // Log error if encoding fails
+                    System.err.println("Failed to save GenerationCenterData: " + error);
+                })
+                .ifPresent(encodedTag -> tag.merge((CompoundTag) encodedTag));
+        return tag;
     }
 
     /**
