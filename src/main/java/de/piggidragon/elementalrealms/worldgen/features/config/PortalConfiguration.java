4e312e72fc8a5d94437c195b9a98d02b1dded9c1
Piggidragon <inf24119@lehre.dhbw-stuttgart.de>
2025-11-10 16:06:48 +0100

    fix: adjusted PortalConfiguration to 1.21.1

--- src/main/java/de/piggidragon/elementalrealms/worldgen/features/config/PortalConfiguration.java
+++ src/main/java/de/piggidragon/elementalrealms/worldgen/features/config/PortalConfiguration.java
@@ -2,7 +2,6 @@ package de.piggidragon.elementalrealms.worldgen.features.config;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import de.piggidragon.elementalrealms.entities.variants.PortalVariant;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
@@ -16,18 +15,13 @@ import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfigur
  * @param portalVariant   Visual appearance of portal
  * @param targetDimension Dimension this portal leads to
  */
-public record PortalConfiguration(PortalVariant portalVariant,
-                                  ResourceKey<Level> targetDimension) implements FeatureConfiguration {
+public record PortalConfiguration(ResourceKey<Level> targetDimension) implements FeatureConfiguration {
 
     /**
      * Codec for JSON serialization of portal configuration.
      */
     public static final Codec<PortalConfiguration> CODEC = RecordCodecBuilder.create(instance ->
             instance.group(
-                    PortalVariant.CODEC
-                            .fieldOf("portal_variant")
-                            .forGetter(config -> config.portalVariant),
-
                     ResourceLocation.CODEC
                             .fieldOf("target_dimension")
                             .forGetter(config -> config.targetDimension.location())
@@ -41,9 +35,8 @@ public record PortalConfiguration(PortalVariant portalVariant,
      * @param portalVariant   Visual variant for portal
      * @param targetDimension Target dimension as ResourceLocation
      */
-    public PortalConfiguration(PortalVariant portalVariant,
-                               ResourceLocation targetDimension) {
-        this(portalVariant, ResourceKey.create(Registries.DIMENSION, targetDimension));
+    public PortalConfiguration(ResourceLocation targetDimension) {
+        this(ResourceKey.create(Registries.DIMENSION, targetDimension));
     }
 
     /**
